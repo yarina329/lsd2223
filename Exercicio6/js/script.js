03e04a0994d259c52e5ced1d3a7ab2dfb38a2b87
mary <68301356+yarina329@users.noreply.github.com>
2023-03-27 20:10:38 +0100

    Atualização do exercicio6 - menu e videos

--- Exercicio6/js/script.js
+++ Exercicio6/js/script.js
@@ -1,3 +1,24 @@
-function mostraConteudoMenu(){
-    document.getElementById('items-mobile').style.display = 'block';
+function mostraConteudoMenu()
+{
+    if (document.getElementById('items-mobile').style.display == 'none')
+    {
+        document.getElementById('texto-menu').innerHTML = '<i class="fa-solid fa-xmark icon-menu-principal"></i>';
+        document.getElementById('items-mobile').style.display = 'block';
+    }
+    else
+    {
+        document.getElementById('texto-menu').innerHTML = '<i class="fa-sharp fa-solid fa-bars icon-menu-principal"></i>';
+        document.getElementById('items-mobile').style.display = 'none';
+    }
+}
+
+function normalizaMenuMobile()
+{
+    largura = window.innerWidth;
+
+    if( largura > 850)
+    {
+        document.getElementById('texto-menu').innerHTML = '<i class="fa-sharp fa-solid fa-bars icon-menu-principal"></i>';
+        document.getElementById('items-mobile').style.display = 'none';
+    }
 }
