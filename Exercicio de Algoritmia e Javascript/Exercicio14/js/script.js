8aa345d7c514f3c9c9737b91e961dfc6824f37ab
yarina329 <yarina329@gmail.com>
2023-05-03 22:27:03 +0100

    Nova Materia

---
+++ Exercicio de Algoritmia e Javascript/Exercicio14/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.your-class').slick({
+        /*slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplaySpeed: 2000,*/
+        autoplay: true,
+        dots: true,
+        pauseOnHover: false
+    });
+});
---
+++ Exercicio de Algoritmia e Javascript/Exercicio15/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.feid').colorbox({
+        width:'50%',
+        rel: 'feid'
+    });
+
+    $('.regista').colorbox({
+        width: 400,
+        height: 300
+    });
+});
