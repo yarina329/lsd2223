6ac508f8329d8cac625be3192de61b6b97dcb41d
yarina329 <yarina329@gmail.com>
2023-06-26 22:15:49 +0100

    Projeto Final - Footer

---
+++ Projeto Final/Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DYAnim</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="font-awesome/css/all.css">
+</head>
+<body>
+    <div>
+        <?php include('rodapes/Footer.php'); ?>
+    </div>
+</body>
+<script src="js/jquery-3.6.4"></script>
+<script src="js/bootstrap/bootstrap.bundle.min.js"></script>
+</html>
---
+++ Projeto Final/rodapes/Footer.php
@@ -0,0 +1,27 @@
+<footer>
+    <div class="row efeito-footer">
+        <div class="col-3 caixa">
+            <img src="imagens/logo.png" alt="Logotipo">
+        </div>
+        <div class="col-3 caixa">
+            <h2>Mais Informações</h2>
+            <a href="#">Contacto</a>
+        </div>
+        <div class="col-3 caixa">
+            <h2>Apoio ao Cliente</h2>
+            <ul>
+                <li><a href="#">Termos de Serviço</a></li>
+                <li><a href="#">Política de Privacidade</a></li>
+                <li><a href="#">Política de Reembolso</a></li>
+                <li><a href="#">Política de Envio</a></li>
+            </ul>
+        </div>
+        <div class="col-3 caixa efeito-cx">
+            <i class="fa-solid fa-phone"></i><p> +351 934470154</p>
+            <i class="fa-regular fa-envelope"></i><p>info@dyanim.com</p>
+        </div>
+    </div>
+</footer>
+<div class="copy">
+    <i class="fa-regular fa-copyright"></i><p>2023 DYAnim</p>
+</div>
