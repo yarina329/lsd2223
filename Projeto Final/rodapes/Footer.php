2ce9f6826e323b2d6497560c930dcdd798b33ca3
yarina329 <yarina329@gmail.com>
2023-07-10 22:36:02 +0100

    Alteração no projeto

--- Projeto Final/rodapes/Footer.php
+++ Projeto Final/rodapes/Footer.php
@@ -1,21 +1,21 @@
 <footer>
-    <div class="row">
-        <div class="col-2 caixa">
+    <div class="row efeit-cx">
+        <div class="col-4">
             <img src="imagens/logo.png" alt="Logotipo">
         </div>
-        <div class="col-8">
-            <div class="col-4 caixa">
+        <div class="row col-8 efeito-cx">
+            <div class="col-4">
                 <h2>Mais Informações</h2>
                 <a href="#">Contacto</a>
             </div>
-            <div class="col-4 caixa">
+            <div class="col-4">
                 <h2>Ajuda</h2>
                 <a href="#">Perguntas Frequentes</a>
             </div>
-            <div class="col-4 caixa efeito-cx">
+            <div class="col-4 cx-mg">
                 <i class="fa-solid fa-phone"></i><p> +351 934470154</p>
                 <i class="fa-regular fa-envelope"></i><p>info@dyanim.com</p>
-                <div class="inst"><i class="fa-brands fa-instagram"></i></div>
+                <div class="footer-icon"><p>Segue-me:</p><i class="fa-brands fa-instagram"></i></div>
             </div>
         </div>
     </div>
