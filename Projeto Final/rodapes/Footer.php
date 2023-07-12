fae82d5bc14a2f4bb4c4b6660ad2fe93c20a5baf
yarina329 <yarina329@gmail.com>
2023-07-12 21:28:01 +0100

    atualizações

--- Projeto Final/rodapes/Footer.php
+++ Projeto Final/rodapes/Footer.php
@@ -1,16 +1,48 @@
+<div class="categoria">
+    <div class="row marg-categ">
+        <div class="col-3">
+            <center>
+                <a href="#" title="Página de Chainsaw Man">
+                    <img src="./imagens/icons-categoria/chainsawMan.jpg">
+                </a>
+            </center>
+        </div>
+        <div class="col-3">
+            <center>
+                <a href="#" title="Página de Demon Slayer">
+                    <img src="./imagens/icons-categoria/demonSlayer.png">
+                </a>
+            </center>
+        </div>
+        <div class="col-3">
+            <center>
+                <a href="#" title="Página de Spy X Family">
+                    <img src="./imagens/icons-categoria/spyXFamily.png">
+                </a>
+            </center>
+        </div>
+        <div class="col-3">
+            <center>
+                <a href="#" title="Página de Naruto">
+                    <img src="./imagens/icons-categoria/naruto.jpg">
+                </a>
+            </center>
+        </div>
+    </div>
+</div>
 <footer>
     <div class="row efeit-cx">
         <div class="col-4">
-            <img src="imagens/logo.png" alt="Logotipo">
+            <img src="imagens/logo.png" title="Logotipo DYA">
         </div>
         <div class="row col-8 efeito-cx">
             <div class="col-4">
                 <h2>Mais Informações</h2>
-                <a href="#">Contacto</a>
+                <a href="#" title="Página de Contacto">Contacto</a>
             </div>
             <div class="col-4">
                 <h2>Ajuda</h2>
-                <a href="#">Perguntas Frequentes</a>
+                <a href="#" title="Página de Perguntas Frequentes">Perguntas Frequentes</a>
             </div>
             <div class="col-4 cx-mg">
                 <i class="fa-solid fa-phone"></i><p>+351 934 470 154</p>
